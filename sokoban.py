dc7206d91ee9ad96d9c81d8872ffdfdff676cde5
Roberto Carlos Islas Islas <97404217+Rob37toIslas@users.noreply.github.com>
2022-04-04 19:58:54 +0000

    estructura corriendo :)

--- sokoban.py
+++ sokoban.py
@@ -1,4 +1,4 @@
-class Sokoban:
+class sokoban:
  #Componentes del juego:
  #0-personaje
  #1-Espacio
@@ -12,60 +12,72 @@ class Sokoban:
   #d-Derecha
   #w-arriba
   #s-abajo
-  personaje=0
-  espacio=1
-  caja=2
-  pared=3
-  meta=4
-  personaje_meta=5
-  caja_meta=6
+  personaje = 0
+  espacio = 1
+  caja = 2
+  pared = 3
+  meta = 4
+  personaje_meta = 5
+  caja_meta = 6
   
-  mapa=[]
+  mapa = []
   
   personaje_fila=0
   personaje_columna=0
   
-  def __init__(self):
-   pass
+    #def __init__(self):
+     #pass
   def leerMapa(self):
-      self.mapa[
-          [1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
-          [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
-          [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
-          [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
-          [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+      self.mapa = [
+            [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+            [3,1,1,1,1,1,1,1,1,3,3,3,3,3,3],
+            [3,1,1,1,1,1,1,0,1,1,1,3,3,4,3],
+            [3,1,1,1,1,1,1,1,1,1,1,1,1,2,3],
+            [3,1,1,1,1,1,1,1,1,1,1,1,1,1,3],
+            [3,3,3,3,3,3,3,1,3,3,3,3,3,3,3],
+       ]
+      self.personaje_columna=7
+      self.personaje_fila=2
     
-      ]
-      self.personaje_columna=1
-      self.personaje_fila=1
-  def imprimirmapa(self):
+  def imprimirMapa(self):
      for fila in self.mapa:
        print(fila)
-  def moverderecha(self):
-      print("Movder deecha")
-    #muñeco espacio
-      if (self.mapa[self.personaje_fila][self.personaje_columna]== 0 
-      and self.mapa[self.personaje_fila][self.personaje_columna +1]==0):
-          self.mapa[self.personaje_fila][self.personaje_columna]=1
+  def mover_derecha(self):
+      print("Mover derecha")
+    #personaje espacio
+      if (self.mapa[self.personaje_fila][self.personaje_columna]== self.personaje 
+      and self.mapa[self.personaje_fila][self.personaje_columna +1]==self.espacio):
+     
+          self.mapa[self.personaje_fila][self.personaje_columna]==self.espacio
+          self.mapa[self.personaje_fila][self.personaje_columna +1]== self.personaje
           self.mapa[self.personaje_fila][self.personaje_columna]=0
           self.personaje_columna+=1
       #Muñeco meta  
-      elif (self.mapa[self.personaje_fila][self.personaje_columna]== 0 
-      and self.mapa[self.personaje_fila][self.personaje_columna +1]==4):
-          self.mapa[self.personaje_fila][self.personaje_columna]=1
-          self.mapa[self.personaje_fila][self.personaje_columna]=5
-          self.personaje_columna+=1
+      #elif (self.mapa[self.personaje_fila][self.personaje_columna]== 0 
+      #and self.mapa[self.personaje_fila][self.personaje_columna +1]==4):
+          #self.mapa[self.personaje_fila][self.personaje_columna]=1
+          #self.mapa[self.personaje_fila][self.personaje_columna]=5
+          #self.personaje_columna+=1
   def jugar (self):
-    instrcciones="a-izquierda...."
-    print(instrcciones)
-    self.leerMapa
-    while true:
-      self.imprimirmapa
-      movimiento= input("a")
-      if movimiento == "d":
-      elif movimiento=="f":
-        print("saliste del juego")
-        
+    instrucciones="""
+    a-izquierda
+    d-derecha
+    x-abajo
+    w-arriba
+    """
+    print(instrucciones)
+    self.leerMapa()
+    while True:
+        self.imprimirMapa()
+        movimiento= input("mover hacia: ")
+        if movimiento == "d":
+         self.mover_derecha()
+        elif movimiento=="f":
+          print("saliste del juego")
+          break
+
+juego=sokoban()
+juego.jugar()
 
     
 
